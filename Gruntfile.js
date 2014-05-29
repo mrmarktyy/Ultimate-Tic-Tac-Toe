ab7b2ddd31eb0a72f1abf0b3a8122042f09367d3
Mark Zhou <mrmarktyy@gmail.com>
2014-05-29 20:35:42 +1000

    [BUILD] Include sass and add grunt sass/watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,22 @@ module.exports = function (grunt) {
             all: ['app/main.js', 'app/*.js', 'app/collections/*.js', 'app/models/*.js', 'app/views/*.js']
         },
 
+        sass: {
+            dist: {
+                files: {
+                    'app/stylesheets/css/site.css': 'app/stylesheets/sass/site.scss',
+                    'app/stylesheets/css/style.css': 'app/stylesheets/sass/style.scss'
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: 'app/stylesheets/**/*.scss',
+                tasks: ['sass']
+            }
+        },
+
         cssmin: {
             minify_all: {
                 options: {
@@ -24,8 +40,8 @@ module.exports = function (grunt) {
                     keepSpecialComments: 0
                 },
                 files: {
-                    'app/stylesheets/site.min.css': ['app/stylesheets/site.css'],
-                    'app/stylesheets/style.min.css': ['app/stylesheets/style.css'],
+                    'app/stylesheets/css/site.min.css': ['app/stylesheets/css/site.css'],
+                    'app/stylesheets/css/style.min.css': ['app/stylesheets/css/style.css'],
                 }
             }
         }
@@ -34,6 +50,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jshint', 'cssmin:minify_all']);
+    grunt.registerTask('default', ['jshint', 'sass:dist']);
+    grunt.registerTask('css', ['watch']);
+    grunt.registerTask('build', ['cssmin:minify_all']);
 };
