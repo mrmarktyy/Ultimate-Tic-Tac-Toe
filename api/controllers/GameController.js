ddc9749abda148843fc2616b6baf04e4f6453fea
Mark Zhou <mrmarktyy@gmail.com>
2014-06-20 22:24:08 +1000

    [REFACTOR] Remove custom logger, improve games maintainence

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -8,7 +8,7 @@
 
 var _       = require('lodash-node');
 var utils   = require('../utils');
-var Logger   = require('../utils/logger');
+var sails   = require('sails');
 var _sockets = {};
 var _games   = {};
 
@@ -16,9 +16,9 @@ function notifyGameAction (uuid, socket_id, action) {
     if (_games[uuid]) {
         // Check which socket to send action
         if (_games[uuid].creator.socket_id === socket_id) {
-            _sockets[_games[uuid].joiner.socket_id].emit('game:move', action);
+            _sockets[_games[uuid].joiner.socket_id].socket.emit('game:move', action);
         } else {
-            _sockets[_games[uuid].creator.socket_id].emit('game:move', action);
+            _sockets[_games[uuid].creator.socket_id].socket.emit('game:move', action);
         }
     }
 }
@@ -26,8 +26,10 @@ function notifyGameAction (uuid, socket_id, action) {
 function notifyGameStart (uuid) {
     if (_games[uuid]) {
         _games[uuid].meta.status = 'in_progress';
-        _sockets[_games[uuid].creator.socket_id].emit('game:start', {role: 2, player: _games[uuid].joiner.player});
-        _sockets[_games[uuid].joiner.socket_id].emit('game:start', {role: 1, player: _games[uuid].creator.player});
+        _sockets[_games[uuid].creator.socket_id].socket.emit('game:start', {role: 2, player: _games[uuid].joiner.player});
+        _sockets[_games[uuid].creator.socket_id].uuid = uuid;
+        _sockets[_games[uuid].joiner.socket_id].socket.emit('game:start', {role: 1, player: _games[uuid].creator.player});
+        _sockets[_games[uuid].joiner.socket_id].uuid = uuid;
     }
 }
 
@@ -35,13 +37,30 @@ module.exports = {
 
     establish: function (session, socket) {
         if (socket) {
-            _sockets[socket.id] = socket;
+            _sockets[socket.id] = {
+                socket: socket
+            };
+            sails.util.debug('[ESTABLISH  ] socket_id: ' + socket.id + ' is connected.');
         }
     },
 
     disconnect: function (session, socket) {
         if (socket) {
+            var uuid = _sockets[socket.id].uuid;
+            sails.util.debug('[DISCONNECT  ] socket_id: ' + socket.id + ' is disconnected.');
             delete _sockets[socket.id];
+            if (uuid && _games[uuid]) {
+                if (_games[uuid].creator.socket_id === socket.id) {
+                    _games[uuid].creator.status = 'inactive';
+                }
+                if (_games[uuid].joiner.socket_id === socket.id) {
+                    _games[uuid].joiner.status = 'inactive';
+                }
+                if (_games[uuid].creator.status === 'inactive' && _games[uuid].joiner.status === 'inactive') {
+                    sails.util.debug('[DISCONNECT  ] game_id: ' + uuid + ' is closed.');
+                    delete _games[uuid];
+                }
+            }
         }
     },
 
@@ -55,7 +74,7 @@ module.exports = {
 
             notifyGameAction(uuid, req.socket.id, action);
         } else {
-            Logger.debug('[ACTION  ] game_id: ' + uuid + ' is invalid');
+            sails.util.debug('[ACTION  ] game_id: ' + uuid + ' is invalid');
             return res.json({
                 status: false,
                 message: 'game_id: ' + uuid + ' is invalid.'
@@ -77,6 +96,7 @@ module.exports = {
                 },
                 creator: {
                     socket_id: req.socket.id,
+                    status: 'active',
                     player: player
                 }
             };
@@ -93,7 +113,7 @@ module.exports = {
             var uuid = req.param('uuid');
             var player = req.param('player');
             if (!_games[uuid] || _games[uuid].joiner) {
-                Logger.debug('[JOIN  ] game_id: ' + uuid + ' is invalid');
+                sails.util.debug('[JOIN  ] game_id: ' + uuid + ' is invalid');
                 return res.json({
                     status: false,
                     message: 'game_id: ' + uuid + ' is invalid'
@@ -104,6 +124,7 @@ module.exports = {
             }
             _games[uuid].joiner = {
                 socket_id: req.socket.id,
+                status: 'active',
                 player: player
             };
             _.defer(function () {
