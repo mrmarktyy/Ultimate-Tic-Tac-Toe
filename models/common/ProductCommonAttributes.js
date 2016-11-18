6c9d9ccff3dd921543634bcc0274b2f4c135089d
Jason Feng <jason.feng@ratecity.com.au>
2016-11-18 16:24:03 +1100

    added auto generate slug

--- models/common/ProductCommonAttributes.js
+++ models/common/ProductCommonAttributes.js
@@ -4,11 +4,11 @@ var Types = keystone.Field.Types;
 module.exports = {
 	name: { type: Types.Text, required: true, initial: true, index: true },
   uuid: { type: Types.Text, initial: true, noedit: true }, // this should be unique, however, team don't have the data yet. will make this unique once all data loaded.
+	slug: { type: Types.Text, index: true },
 	otherNames: { type: Types.TextArray },
 	displayName: { type: Types.Text, required: true, initial: true },
 	isDiscontinued: { type: Types.Boolean, indent: true, default: false },
 	isFeaturedProduct: { type: Types.Boolean, indent: true, default: false },
 	isPromotedProduct: { type: Types.Boolean, indent: true, default: false },
-	slug: { type: Types.Text },
   visibility: { type: Types.Select, options: ['seo', 'none', 'all'], default: 'all', required: true, initial: true },
 };
