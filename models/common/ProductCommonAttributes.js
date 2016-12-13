ae605cd0a07e50daa1706fe8a3e5da69c1d7d05d
Jason Feng <jason@foxlime.io>
2016-12-14 10:52:19 +1100

    added(copied) winston logger (#78)
    * added(copied) winston logger
    
    * credit card field to camel case
    
    * added new relic no config setting for dev

--- models/common/ProductCommonAttributes.js
+++ models/common/ProductCommonAttributes.js
@@ -3,12 +3,12 @@ var Types = keystone.Field.Types;
 
 module.exports = {
 	name: { type: Types.Text, required: true, initial: true, index: true },
-  uuid: { type: Types.Text, initial: true }, // this should be unique, however, team don't have the data yet. will make this unique once all data loaded.
+	uuid: { type: Types.Text, initial: true }, // this should be unique, however, team don't have the data yet. will make this unique once all data loaded.
 	slug: { type: Types.Text, index: true },
 	otherNames: { type: Types.TextArray },
 	displayName: { type: Types.Text, required: true, initial: true },
 	isDiscontinued: { type: Types.Boolean, indent: true, default: false },
 	isFeaturedProduct: { type: Types.Boolean, indent: true, default: false },
 	isPromotedProduct: { type: Types.Boolean, indent: true, default: false },
-  visibility: { type: Types.Select, options: ['seo', 'none', 'all'], default: 'all', required: true, initial: true },
+	visibility: { type: Types.Select, options: ['seo', 'none', 'all'], default: 'all', required: true, initial: true },
 };
