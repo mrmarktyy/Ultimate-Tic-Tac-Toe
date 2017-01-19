c9950655a1770030e8d4dde0d8444b31d5893912
Gareth Robinson <gareth.robinson@ratecity.com.au>
2017-01-19 15:17:20 +1100

    Add redirects model

---
+++ models/redirect/Redirect.js
@@ -0,0 +1,20 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var Redirect = new keystone.List('Redirect');
+
+Redirect.add({
+  from: { type: Types.Text, required: true, initial: true, unique: true },
+  to: { type: Types.Text, required: true, initial: true },
+  status: {
+    type: Types.Select,
+    options: [{value: '301', label: '301 - Permanent'}, {value: '302', label: '302 - Temporary'}],
+    required: true,
+    initial: true,
+  },
+});
+
+Redirect.defaultColumns = 'from, to, status';
+Redirect.track = true;
+Redirect.register();
+
