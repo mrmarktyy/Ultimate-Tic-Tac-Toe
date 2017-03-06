2e70134e3adcce26177d6984a8cdb540df4f4797
Ian Fletcher <ianfletcher9@gmail.com>
2017-03-06 14:49:56 +1100

    Last piece in salesforce homeloan integration

--- models/helpers/salesforceVerticals.js
+++ models/helpers/salesforceVerticals.js
@@ -1,4 +1,6 @@
+// Homeloans originally from SORBET were handled on the variation level
 module.exports = {
   'Car Loans': 'PersonalLoan',
   'Personal Loans': 'PersonalLoan',
+  'Home Loans': 'HomeLoanVariation',
 }
