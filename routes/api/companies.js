08bd9dd63208b63ede3f0839224a8150925549ba
mahendra <1987mahendrad@gmail.com>
2017-03-30 16:08:24 +1100

    add click counts to companies api

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -1,28 +1,52 @@
 var keystone = require('keystone')
 
 var Company = keystone.list('Company')
+var HomeLoanVariaiton = keystone.list('HomeLoanVariation')
 var PersonalLoan = keystone.list('PersonalLoan')
 var CompanyPersonalLoan = keystone.list('CompanyPersonalLoan')
 var CreditCard = keystone.list('CreditCard')
 var CompanyService = require('../../services/CompanyService')
+const MULTIPLIER = 5.32
 
 exports.list = function (req, res) {
 
 	let promise = Company.model.find().lean().exec()
-
 	let response = {}
 	let countPromises = []
+
 	promise.then((companies) => {
 		companies.forEach((company) => {
 			company = CompanyService.fixLogoUrl(company)
 
 			response[company._id] = Object.assign(company, {
 				verticals: {
+					homeLoans: {},
 					personalLoans: {},
 					carLoans: {},
 					creditCards: {},
 				},
 			})
+
+			let hlPromise = HomeLoanVariaiton.model.count({
+				company: company._id,
+			}).exec((err, count) => {
+				if (err) return 'database error'
+				response[company._id].verticals.homeLoans.count = count
+			})
+			countPromises.push(hlPromise)
+
+			let hlccPromise = HomeLoanVariaiton.model
+				.find({company: company._id, monthlyClicks: {$gt: 0}})
+				.exec((err, results) => {
+					let totalClicks = 0
+					results.forEach((result) => {
+						totalClicks = totalClicks + result.monthlyClicks
+					})
+
+					response[company._id].verticals.homeLoans.popularityScore = totalClicks * MULTIPLIER
+				})
+
+			countPromises.push(hlccPromise)
 
 			let plPromise = PersonalLoan.model.count({
 				company: company._id,
