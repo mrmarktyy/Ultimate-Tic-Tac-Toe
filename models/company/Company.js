e63250f5db8d304622a32c80a0e1d984ea948fd6
Ian Fletcher <ianfletcher9@gmail.com>
2017-07-06 16:29:51 +1000

    added hasBig4Comparison to Company

--- models/company/Company.js
+++ models/company/Company.js
@@ -28,6 +28,7 @@ Company.add({
 	afsl: { type: Types.Number },
 	uuid: { type: Types.Text, initial: true, noedit: true }, // this should be unique, however, team don't have the data yet. will make this unique once all data loaded.
 	slug: { type: Types.Text, unique: true, required: true, initial: true },
+	hasBig4Comparison: { type: Types.Boolean, default: false, indent: true },
 	legacyCode: { type: Types.Text, noedit: true },
 	url: { type: Types.Url, required: true, index: true, initial: true },
 	searchKeyword: { type: Types.TextArray },
