aeb2db993c55ab492c7176a99856ffefe52ebe2f
Ian Fletcher <ianfletcher9@gmail.com>
2017-08-17 15:51:31 +1000

    add slug to modal homeloan variation

--- models/common/ProductCommonAttributes.js
+++ models/common/ProductCommonAttributes.js
@@ -4,7 +4,7 @@ const Types = keystone.Field.Types
 module.exports = {
 	name: { type: Types.Text, required: true, initial: true, index: true },
 	uuid: { type: Types.Text, initial: true, index: true, unique: true },
-	slug: { type: Types.Text, index: true },
+	slug: { type: Types.Text, index: true, initial: true},
 	otherNames: { type: Types.TextArray },
 	displayName: { type: Types.Text, initial: true },
   isMonetized: { type: Types.Boolean, indent: true, noedit: true, default: false },
