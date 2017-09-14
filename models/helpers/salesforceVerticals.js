0b1fd08b69196c91294f8e61ae16555b4c4ae6b9
Ian Fletcher <ianfletcher9@gmail.com>
2017-09-14 11:48:05 +1000

    add monetization to savings accounts

--- models/helpers/salesforceVerticals.js
+++ models/helpers/salesforceVerticals.js
@@ -3,9 +3,10 @@ var _ = require('lodash')
 var genericVerticals = require('../helpers/genericVerticals')
 
 const specificVerticals = {
-  'Car Loans': { collection: 'PersonalLoan', findClause: { isCarLoan: 'YES' } },
-	'Personal Loans': { collection: 'PersonalLoan', findClause: { isPersonalLoan: 'YES' } },
-  'Home Loans': { collection: 'HomeLoanVariation' },
+  'Car Loans': { collection: 'PersonalLoan', findClause: { isCarLoan: 'YES' }, salesforceVertical: 'Car Loans' },
+	'Personal Loans': { collection: 'PersonalLoan', findClause: { isPersonalLoan: 'YES' }, salesforceVertical: 'Personal Loans' },
+  'Home Loans': { collection: 'HomeLoanVariation', salesforceVertical: 'Home Loans' },
+  'Savings Accounts': { collection: 'SavingsAccount', salesforceVertical: 'Savings A/C' },
 }
 
 module.exports = _.merge({}, specificVerticals, genericVerticals)
