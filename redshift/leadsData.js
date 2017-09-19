f6d26d26871cded6e1fff5e078245d43cbcd3896
Priya Choudhary <priya1091992@gmail.com>
2017-09-19 13:28:59 +0530

    changing endDate (last day of the month selected) to the first day of the next month.

--- redshift/leadsData.js
+++ redshift/leadsData.js
@@ -6,7 +6,7 @@ exports.leadsCsv = async (broker, month, year) => {
 	let yearValue = year || moment().year()
 	let date = moment(`1-${month}-${yearValue}`, 'DD-MMM-YYYY')
 	let startDate = date.format('YYYY-MM-DD')
-	let endDate = moment(startDate).endOf('month').format('YYYY-MM-DD')
+	let endDate = moment(startDate).add(1, 'months').startOf('month').format('YYYY-MM-DD')
 	let command = `
 		SELECT * FROM user_homeloan_leads
 		WHERE created_at between $1 and $2 and broker_slug = $3
