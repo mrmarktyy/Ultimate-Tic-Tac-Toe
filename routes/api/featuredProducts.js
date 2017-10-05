90c13a9edf0ba9fd172601792aad97aa9cfdb46c
mahendra <1987mahendrad@gmail.com>
2017-10-05 15:27:00 +1100

    pages attribute

--- routes/api/featuredProducts.js
+++ routes/api/featuredProducts.js
@@ -12,7 +12,7 @@ exports.list = async function (req, res) {
         { dateStart: { $lte: datenow }, enabled: true, dateEnd: { $gte: datenow }, sortOrder: { $ne: null } },
       ]
     }, {
-      uuid: 1, vertical: 1, title: 1, description: 1, sortOrder: 1, dateStart: 1, image: 1, shareOfVoiceMatrix: 1, shareOfVoiceType: 1, shareOfVoiceValue: 1
+      uuid: 1, vertical: 1, title: 1, description: 1, sortOrder: 1, dateStart: 1, pages: 1, image: 1, shareOfVoiceMatrix: 1, shareOfVoiceType: 1, shareOfVoiceValue: 1
     }
   ).lean().exec()
 
