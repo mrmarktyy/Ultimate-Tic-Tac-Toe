22897125363da45aaf0d29b95316400b5df305ab
Ian Fletcher <ianfletcher9@gmail.com>
2018-01-16 14:17:05 +1100

    only one active special for a credit card

--- models/credit-card/CreditCardSpecial.js
+++ models/credit-card/CreditCardSpecial.js
@@ -25,9 +25,46 @@ CreditCardSpecial.add({
   },
 })
 
-CreditCardSpecial.schema.pre('validate', function (next) {
+CreditCardSpecial.schema.pre('validate', async function (next) {
   if (!!this.endDate && this.startDate > this.endDate) {
     next(Error('Start date cannot be past the end date.'))
+  }
+  let currentDate = new Date()
+  if (this.startDate <= currentDate && (((!!this.endDate && this.endDate >= currentDate) || !this.endDate))) {
+    let specials = []
+
+    if (this.product) {
+      specials = await keystone.list('CreditCardSpecial').model.find({
+        product: this.product,
+        _id: {$ne: this.id},
+        $or: [
+          {startDate: {$lte: new Date()}, $and: [{endDate: {$exists: true}}, {endDate: {$gte: new Date()}}]},
+          {startDate: {$lte: new Date()}, endDate: {$exists: false}},
+        ],
+      }).populate('company product').lean().exec()
+    } else {
+      specials = await keystone.list('CreditCardSpecial').model.find({
+        product: {$ne: null},
+        _id: {$ne: this.id},
+        $or: [
+          {startDate: {$lte: new Date()}, $and: [{endDate: {$exists: true}}, {endDate: {$gte: new Date()}}]},
+          {startDate: {$lte: new Date()}, endDate: {$exists: false}},
+        ],
+      }).populate('company product').lean().exec()
+    }
+    let companySpecials = await keystone.list('CreditCardSpecial').model.find({
+      company: this.company,
+      product: null,
+      _id: {$ne: this.id},
+      $or: [
+        {startDate: {$lte: new Date()}, $and: [{endDate: {$exists: true}}, {endDate: {$gte: new Date()}}]},
+        {startDate: {$lte: new Date()}, endDate: {$exists: false}},
+      ],
+    }).populate('company').lean().exec()
+    specials = specials.concat(companySpecials)
+    if (specials.length) {
+      next(Error(`There is already an active special Company: ${specials[0].company.name} ${specials[0].product ? ' Name: ' + specials[0].product.name : ''}`))
+    }
   }
   next()
 })
