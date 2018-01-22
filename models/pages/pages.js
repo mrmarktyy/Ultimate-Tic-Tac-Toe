91408a2d5a009fb898f6918cf8caf789441056f0
Prince Soni <prince.soni@tothenew.com>
2018-01-22 12:04:29 +0530

    Updated pages model [Fix #154465235]

--- models/pages/pages.js
+++ models/pages/pages.js
@@ -7,19 +7,20 @@ var verifiedCommonAttribute = require('../common/verifiedCommonAttribute')
 
 const Pages = new keystone.List('Pages', {track: true}).add({
 	uuid: {type: Types.Text, initial: true, unique: true},
-	url: {type: Types.Text, initial: true, unique: true},
+	url: {type: Types.Text, initial: true, unique: true,  noedit: true},
 	header: {type: Types.Text, initial: true},
 	tagline: {type: Types.Text, initial: true},
 	title: {type: Types.Text, initial: true},
 	popularSearchTitle: {type: Types.Text},
 	vertical: {type: Types.Select, options: verticals, initial: true, default: 'default'},
 	resultName: {type: Types.Text},
+	variant: {type: Types.Text},
 	description: {type: Types.Code, height: 250, language: 'html'},
 	keywords: {type: Types.Text},
 	canonical: {type: Types.Text},
 	category: {type: Types.TextArray},
 	userJourneyStage: {type: Types.Text},
-	searchVolume: { type: Number, min: 0 },
+	rankingScore: { type: Number, min: 0 },
 	og: {
 		id: {type: Types.Text},
 		title: {type: Types.Text},
