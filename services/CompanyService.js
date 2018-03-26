9cea9541f2dc77425da2775368883645526a0cee
Mark Zhou <mrmarktyy@gmail.com>
2018-03-26 12:07:35 +1100

    (bug) remove protocol for company logo url

--- services/CompanyService.js
+++ services/CompanyService.js
@@ -7,10 +7,10 @@ const bankTypes = [
 exports.fixLogoUrl = function (company) {
 	if (company.logo) {
 		if (company.logo.url) {
-			company.logo.url = company.logo.url.replace('res.cloudinary.com', '//production-ultimate-assets.ratecity.com.au')
+			company.logo.url = company.logo.url.replace('http://res.cloudinary.com', '//production-ultimate-assets.ratecity.com.au')
 		}
 		if (company.logo.secure_url) {
-			company.logo.secure_url = company.logo.secure_url.replace('res.cloudinary.com', '//production-ultimate-assets.ratecity.com.au')
+			company.logo.secure_url = company.logo.secure_url.replace('https://res.cloudinary.com', '//production-ultimate-assets.ratecity.com.au')
 		}
 	}
 	return company
