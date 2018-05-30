1954a97841f121bb3f10998bb159d7402cd2246b
Prince Soni <prince.soni@tothenew.com>
2018-05-30 16:03:44 +0530

    Fixed http issue on broker pages

--- routes/api/brokers.js
+++ routes/api/brokers.js
@@ -3,15 +3,22 @@ var Broker = keystone.list('Broker')
 var CompanyService = require('../../services/CompanyService')
 
 exports.list = async function (req, res) {
-	let brokers = await Broker.model.find({ $or: [ { isDiscontinued: false }, { isDiscontinued: {$exists: false} } ] }).populate('tiles testimonials companies').lean().exec()
+	let brokers = await Broker.model.find({$or: [{isDiscontinued: false}, {isDiscontinued: {$exists: false}}]}).populate('tiles testimonials companies').lean().exec()
 	brokers = brokers.map((broker) => {
-		broker.logo = broker.logo && broker.logo.url
+		broker.logo = broker.logo && fixUrl(broker.logo.url)
 		broker.imageHeader = broker.imageHeader && broker.imageHeader.url
 		if (broker.companies) {
 			broker.companies = broker.companies.map((company) => CompanyService.fixLogoUrl(company))
 		}
+		broker.tiles.map(tile => {
+			tile.icon = fixUrl(tile.icon)
+			return tile
+		})
 		return broker
 	})
 	res.jsonp(brokers)
 }
 
+function fixUrl(url) {
+	return url.replace('http://', '//')
+}
