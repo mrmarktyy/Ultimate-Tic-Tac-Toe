9f7b503434704e119a031e6ee6c08a78dce168a7
Ian Fletcher <ianfletcher9@gmail.com>
2018-10-12 09:11:25 +1100

    fixed company details in discontinued homeloan revert report

--- services/reporting/dataReportAttachments/discontinuedHomeLoanRevert.js
+++ services/reporting/dataReportAttachments/discontinuedHomeLoanRevert.js
@@ -7,7 +7,7 @@ const json2csv = require('json2csv')
 const HomeLoanVariation = keystoneShell.list('HomeLoanVariation')
 
 module.exports = async function discontinuedHomeLoanRevert (filePath) {
-  let variations = await HomeLoanVariation.model.find({isDiscontinued: false, revertVariation: {$ne:null}}).populate('revertVariation Company Product')
+  let variations = await HomeLoanVariation.model.find({isDiscontinued: false, revertVariation: {$ne: null}}).populate('revertVariation company product')
   let discontinued = variations.filter((variation) => {
     return variation.revertVariation.isDiscontinued === true
   })
