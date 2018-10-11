1170cbc5b5155645ef0784ac9d1513f369385e37
Ian Fletcher <ianfletcher9@gmail.com>
2018-10-11 16:34:22 +1100

    added email data report discontinued revertVariations

---
+++ services/reporting/dataReportAttachments/discontinuedHomeLoanRevert.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+var keystoneShell = require('../../../utils/keystoneShell')
+const productService = require('../../productService')
+const fs = require('fs')
+const json2csv = require('json2csv')
+const HomeLoanVariation = keystoneShell.list('HomeLoanVariation')
+
+module.exports = async function discontinuedHomeLoanRevert (filePath) {
+  let variations = await HomeLoanVariation.model.find({isDiscontinued: false, revertVariation: {$ne:null}}).populate('revertVariation Company Product')
+  let discontinued = variations.filter((variation) => {
+    return variation.revertVariation.isDiscontinued === true
+  })
+  let records = []
+  discontinued.forEach((record) => {
+    let obj = {
+      companyName: record.company.name,
+      companyUUID: record.company.uuid,
+      variationName: record.name,
+      variationUUID: record.uuid,
+      revertVariationName: record.revertVariation.name,
+      revertVariationUUID: record.revertVariation.uuid,
+    }
+    records.push(obj)
+  })
+
+  let result = null
+  if (records.length) {
+    let csv = json2csv({data: records})
+    let fileName = `discontinued_hl_revertvariations.csv`
+    fs.writeFileSync(filePath + fileName, csv)
+    result =  {path: `${filePath}${fileName}`}
+  }
+  return result
+}
