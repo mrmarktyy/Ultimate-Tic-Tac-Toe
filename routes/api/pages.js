e17780736bd2afa7d1b7ac2432fdb68d45fc2252
Prince Soni <prince.soni@tothenew.com>
2018-10-12 12:18:28 +0530

    Updated faqs for longtailux

--- routes/api/pages.js
+++ routes/api/pages.js
@@ -6,7 +6,7 @@ exports.list = async function (req, res) {
 	if (req.query.url) {
 		condition.url = req.query.url
 	}
-	let pages = await Pages.model.find(condition, {_id: 0}).populate('links longTailArticles longTailSimilarSearches longTailPopularSearches longTailFaqs').lean().exec()
+	let pages = await Pages.model.find(condition, {_id: 0}).populate('links longTailArticles longTailSimilarSearches longTailPopularSearches').lean().exec()
 	res.jsonp(pages)
 }
 
