007bd77f7fb317c9cdd84205fac322530102d13b
xing <xing.gou@ratecity.com.au>
2019-06-25 15:39:20 +1000

    query payment from r_orders in 2node
    fix

--- redshift/importPaymentMonetizationTypes.js
+++ redshift/importPaymentMonetizationTypes.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const logger = require('../utils/logger')
-const redshiftQuery = require('../utils/redshiftQuery')
+const redshiftQuery = require('../utils/ratecityRedshiftQuery')
 const keystoneShell = require('../utils/keystoneShell')
 const mongoosePromise = require('../utils/mongoosePromise')
 const Monetize = keystoneShell.list('Monetize')
@@ -9,22 +9,32 @@ const Monetize = keystoneShell.list('Monetize')
 module.exports = async function () {
   let connection = await mongoosePromise.connect()
   try {
-    let sqlCommand = 'select s1.product_uuid, s1.CPC_price, s1.CPA_price, s1.CPAA_price, s1.CPL_price FROM orders s1 JOIN (SELECT product_uuid, MAX(date_start) date_start FROM orders GROUP BY product_uuid) s2 on s1.product_uuid = s2.product_uuid AND s1.date_start = s2.date_start AND s1.active = True'
-
+		let sqlCommand = `
+			select s1.product_uuid,
+				 case lower(monetisation_type)
+						 when 'cpa/cpaa' then 'cpa'
+						 when 'cpaa' then 'cpa'
+						 else lower(monetisation_type)
+				 end monetisation_type
+			from r_orders s1
+			inner join (
+					select product_uuid, min(date_start) date_start
+					from r_orders
+					where monetisation_type in ('CPC', 'CPA', 'CPAA', 'CPA/CPAA', 'CPL')
+					and product_uuid <> ''
+					and order_op_subline_status = true
+					group by product_uuid) s2
+			on s1.product_uuid = s2.product_uuid
+			and s1.date_start = s2.date_start
+			and s1.order_op_subline_status = true
+			and s1.product_uuid <> ''
+		`
     let products = await redshiftQuery(sqlCommand, [])
     let promises = []
     products.forEach((product) => {
-      let paymentType = 'cpc'
-
-      if (product.cpa_price || product.cpaa_price) {
-        paymentType = 'cpa'
-      }
-      if (product.cpl_price) {
-        paymentType = 'cpl'
-      }
       promises.push(Monetize.model.update(
         {uuid: product.product_uuid},
-        {$set: {paymentType: paymentType}},
+        {$set: {paymentType: product.monetisation_type}},
         {upsert: false}
       ))
 
