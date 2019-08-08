91a20d737be5f02a2c097f5ca93f6934e92227a8
Ian Fletcher <ianfletcher9@gmail.com>
2019-08-08 12:50:56 +1000

    partnerproduct api

---
+++ routes/api/partnerProducts.js
@@ -0,0 +1,13 @@
+const keystone = require('keystone')
+const removeUneededFields = require('../../utils/removeUneededFields')
+
+const PartnerProduct = keystone.list('PartnerProduct')
+
+exports.list = async function (req, res) {
+  const partnerProducts = await PartnerProduct.model.find(
+      { isDiscontinued: false },
+      { updatedBy: 0, _id: 0, updatedAt: 0, createdBy: 0, createdAt: 0, __v: 0 },
+    ).populate({path: 'partners', select: '-_id -updatedBy -updatedAt -createdBy -isDiscontinued -__v'})
+    .lean()
+  res.jsonp(partnerProducts)
+}
