58f28c149be779dec6c8179d4720d1d78204330d
Ian Fletcher <ianfletcher9@gmail.com>
2019-08-14 11:49:07 +1000

    added notes and company to partner products

--- routes/api/partnerProducts.js
+++ routes/api/partnerProducts.js
@@ -5,7 +5,7 @@ const PartnerProduct = keystone.list('PartnerProduct')
 exports.list = async function (req, res) {
   const partnerProducts = await PartnerProduct.model.find(
       { isDiscontinued: false },
-      { updatedBy: 0, _id: 0, updatedAt: 0, createdBy: 0, createdAt: 0, __v: 0 },
+      { updatedBy: 0, _id: 0, updatedAt: 0, createdBy: 0, createdAt: 0, __v: 0, notes: 0 },
     ).populate({path: 'partners', select: '-_id -updatedBy -updatedAt -createdBy -isDiscontinued -__v'})
     .lean()
   res.jsonp(partnerProducts)
