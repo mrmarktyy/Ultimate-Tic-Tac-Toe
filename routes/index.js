0b3bb963dd66f75e316f6e1a81587a0ea3f6d9c6
Ian <ianfletcher9@gmail.com>
2019-12-23 10:58:45 +1100

    added product tallies api

--- routes/index.js
+++ routes/index.js
@@ -148,6 +148,8 @@ exports = module.exports = function (app) {
   
   // Partner Products
   app.get('/api/partner-products', keystone.middleware.api, routes.api.partnerProducts.list)
-
   app.get('/api/partners', keystone.middleware.api, routes.api.partner.list)
+
+  //product tallies
+  app.get('/api/product-tallies', keystone.middleware.api, routes.api.productTallies.list)
 }
